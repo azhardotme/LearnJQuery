23efa9fdfb50c5d7005dd76d5e15161b239e13cc
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2020-11-29 02:15:04 +0600

    Added some program

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert("hd");
+//html selector
+$(".my-div h1").text("Hello");
+$("#p3,h1").text("Goodbye");
+$("#p2").text("Welcome");
+
+//text manipulation and some method
+$("#p1").html("Good Day");
+$("#p3").append(" Welcome");
+$("#p1").prepend("Welcome");
+
+//attribute manipulation
+$("a").attr("href","https://www.google.com/");
+
+//CSS Style manipulating
+
+$("h3").css("color","red");
+$("h3").css("font-size","4rem");
+
+$("h2").css({"color":"green","font-size":"2rem"});
+
+$("h4").addClass("style1 style2");
